cad83fa740ecbff61629359450ab476ea3d6a9eb
Anne O'Leary <anne272@gmail.com>
2023-08-07 18:13:58 +0100

    Added placeholder info to App.js file

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <header className="App-header">
+        {/* Deleted the text that automatically appears here to add my own*/}
+        <h1>All about us!</h1>        
+        <h2>Anne</h2>              
+        
       </header>
+
+      <ul className="App-ordered_list">
+          <li>My favourite cuisine is Vietnamese but for desserts always head to Italy</li>
+          <li>Outside the kitchen I like to run and cross-stitch.</li>
+          
+        </ul>
+      
+
     </div>
   );
 }

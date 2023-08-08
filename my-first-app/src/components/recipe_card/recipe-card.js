1ae31dbc13ec586d29e1bd22b294a811222c4f59
Sophia John <smc_john@hotmail.com>
2023-08-08 01:03:40 -0400

    Added Notes button and hide notes

--- my-first-app/src/components/recipe_card/recipe-card.js
+++ my-first-app/src/components/recipe_card/recipe-card.js
@@ -1,54 +1,59 @@
 import React, { useState } from 'react';
 
 const RecipeCard = () => {
-  const [hiddenInstructions, setHiddenInstructions] = useState(true);
+  const [showNotes, setShowNotes] = useState(false);
+  const [notes, setNotes] = useState('');
 
-  const toggleInstructions = () => {
-    setHiddenInstructions(prevHidden => !prevHidden);
+  const toggleNotes = () => {
+    setShowNotes(prevShowNotes => !prevShowNotes);
+  };
+
+  const handleNotesChange = event => {
+    setNotes(event.target.value);
   };
 
   return (
     <div className="recipe-card">
       <h2>Peach Cobbler</h2>
-      <button className="edit-button" onClick={toggleInstructions}>
-        {hiddenInstructions ? 'Show Instructions' : 'Hide Instructions'}
-      </button>
-      {hiddenInstructions ? null : (
-        <div className="instructions">
-          <h3>Ingredients:</h3>
-          <ul className="ingredients-list">
-            <li>5 peaches, peeled, cored and sliced (about 4 cups)</li>
-            <li>3/4 cup granulated sugar</li>
-            <li>1/4 teaspoon salt</li>
-            <li>6 Tablespoons butter</li>
-            <li>1 cup all-purpose flour</li>
-            <li>1 cup granulated sugar</li>
-            <li>2 teaspoons baking powder</li>
-            <li>3/4 cup milk</li>
-            <li>Ground cinnamon</li>
-          </ul>
+      <img src="my-first-app/src/images/peachcobbler.jpeg" alt="Peach Cobbler" className="recipe-img" />
 
-          <h3>Instructions:</h3>
-          <ol className="instructions">
-            <li>Add the sliced peaches, sugar, and salt to a saucepan and stir to combine.</li>
-            <li>Cook on medium heat for just a few minutes, until the sugar is dissolved and helps to bring out juices from the peaches. Remove from heat and set aside.</li>
-            <li>Preheat oven to 350 degrees F. Slice butter into pieces and add to a 9x13 inch baking dish. Place the pan in the oven while it preheats, to allow the butter to melt. Once melted, remove the pan from the oven.</li>
-            <li>In a large bowl mix together the flour, sugar, baking powder, and salt. Stir in the milk, just until combined. Pour the mixture into the pan, over the melted butter and smooth it into an even layer.</li>
-            <li>Spoon the peaches and juice (or canned peaches, if using) over the batter. Sprinkle cinnamon generously over the top.</li>
-            <li>Bake at 350 degrees for about 38-40 minutes. Serve warm, with a scoop of ice cream, if desired.</li>
-          </ol>
+      <h3>Ingredients:</h3>
+      <ul className="ingredients-list">
+        <li>5 peaches, peeled, cored and sliced (about 4 cups)</li>
+        <li>3/4 cup granulated sugar</li>
+        <li>1/4 teaspoon salt</li>
+        <li>6 Tablespoons butter</li>
+        <li>1 cup all-purpose flour</li>
+        <li>1 cup granulated sugar</li>
+        <li>2 teaspoons baking powder</li>
+        <li>3/4 cup milk</li>
+        <li>Ground cinnamon</li>
+      </ul>
 
-          <h3>Notes:</h3>
-          <p>To substitute canned peaches, use 1 quart jar, undrained.</p>
+      <h3>Instructions:</h3>
+      <ol className="instructions">
+        <li>Add the sliced peaches, sugar, and salt to a saucepan and stir to combine.</li>
+        <li>Cook on medium heat for just a few minutes, until the sugar is dissolved and helps to bring out juices from the peaches. Remove from heat and set aside.</li>
+        <li>Preheat oven to 350 degrees F. Slice butter into pieces and add to a 9x13 inch baking dish. Place the pan in the oven while it preheats, to allow the butter to melt. Once melted, remove the pan from the oven.</li>
+        <li>In a large bowl mix together the flour, sugar, baking powder, and salt. Stir in the milk, just until combined. Pour the mixture into the pan, over the melted butter and smooth it into an even layer.</li>
+        <li>Spoon the peaches and juice (or canned peaches, if using) over the batter. Sprinkle cinnamon generously over the top.</li>
+        <li>Bake at 350 degrees for about 38-40 minutes. Serve warm, with a scoop of ice cream, if desired.</li>
+      </ol>
 
-          <h3>Nutrition:</h3>
-          <ul className="nutrition-list">
-            <li>Calories: 315kcal</li>
-            <li>Carbohydrates: 59g</li>
-            <li>Protein: 3g</li>
-          </ul>
+      {showNotes && (
+        <div className="notes-section">
+          <textarea
+            className="notes-input"
+            value={notes}
+            onChange={handleNotesChange}
+            placeholder="Add your notes here..."
+          />
         </div>
       )}
+
+      <button className="add-notes-button" onClick={toggleNotes}>
+        {showNotes ? 'Hide Notes' : 'Add Notes'}
+      </button>
     </div>
   );
 };

b7287a06406234743f2915e2f849ec3311ab994a
sloukelly <sarahloukelly@gmail.com>
2023-08-10 16:11:45 +0100

    Added my own info and reasons
    I hope Sophia likes toast and reading

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -31,27 +31,39 @@ function App() {
           <Route path="/about-us" element={<div className="App-ordered_list">
             <h2>Anne</h2>
             <ul>
-              <li>My favourite cuisine is Vietnamese but for desserts always head to Italy</li>
+              <li>My favourite cuisine is Vietnamese but for desserts always head to Italy.</li>
               <li>Outside the kitchen I like to run and cross-stitch.</li>
+              <li>I like running because it is a reason to go outside whils staying healthy.</li>
             </ul>
 
             <h2>Beth</h2>
             <ul>
-              <li>I love watching tv shows and listening to music</li>
-              <li>When I'm not chilling in bed.. I go to boxing, something I have recently taken up</li>
+              <li>I love watching tv shows and listening to music.</li>
+              <li>When I'm not chilling in bed.. I go to boxing, something I have recently taken up.</li>
+              <li>I like boxing because it is active and requires working with others.</li>
             </ul>
 
             <h2>Georgia</h2>
             <ul>
               <li>My favourite food is Chinese but I do love a pizza!</li>
               <li>My hobbies include rock climbing, snowboarding and playing with my rabbits.</li>
+              <li>I like rock climbing because it requires problem solving skills as well as physical strength.</li>
             </ul>
 
             <h2>Sophia</h2>
             <ul>
-              <li>My favorite food is ...</li>
-              <li>I like to ...</li>
+              <li>My favorite food is toast.</li>
+              <li>I like to read books.</li>
+              <li> I like reading because it has the potential to transport you to a whole new world.</li>
             </ul>
+
+            <h2>Sarah</h2>
+            <ul>
+              <li>My favorite food is Mexican! As long as it is vegan.</li>
+              <li>I like to look after my plants, go charity shopping, and go jogging or to the gym.</li>
+              <li> I like looking after plants as it's interesting to learn about their different needs and they brighten up a room.</li>
+            </ul>
+
           </div>} />
         </Routes>
       </div>

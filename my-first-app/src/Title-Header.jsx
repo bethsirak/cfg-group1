6faf33cf9d8025a6ff04982ca13e728112b37f72
geo4gia <g.b.e-dwards@hotmail.co.uk>
2023-08-18 22:12:15 +0100

    Update Title-Header.jsx

--- my-first-app/src/Title-Header.jsx
+++ my-first-app/src/Title-Header.jsx
@@ -3,10 +3,18 @@ import React from "react";
 export default function Title(){
     return(
         <div>
-            <svg>
-            <circle cx={50} cy={50} r={10} fill="red" />
+            <svg height="130" width="500">
+                <defs>
+                    <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+                    <stop offset="0%" style="stop-color:rgb(255,255,0);stop-opacity:1" />
+                    <stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+                    </linearGradient>
+                </defs>
+                <ellipse cx="100" cy="70" rx="85" ry="55" fill="url(#grad1)" />
+                <text fill="#ffffff" font-size="45" font-family="Verdana" x="50" y="86">The World's Delights</text>
+                Sorry, your browser does not support inline SVG.
             </svg>
-            <h1 className="title">the World's Delights</h1>
+            
         </div>
     )
 }

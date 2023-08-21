48a5be37d760092a2f9620ef9dafd6b31c16082f
geo4gia <g.b.e-dwards@hotmail.co.uk>
2023-08-21 22:35:04 +0100

    continents pages

--- my-first-app/src/components/Continents/Caribbean.jsx
+++ my-first-app/src/components/Continents/Caribbean.jsx
@@ -3,7 +3,20 @@ import React from "react";
 export default function Carribbean(){
     return(
         <div>
-            <h1>Hi, this is the Caribbean</h1>
+            <h1>Caribbean Cuisine</h1>
+            <p>
+                As a result of the colonization, the Caribbean is a fusion of multiple sources; British, Spanish, Dutch and French colonized the area and 
+                brought their respective cuisines that mixed with West African as well as Amerindian, East Asian, Arab, South Asian influences from enslaved, 
+                indentured and other laborers brought to work on the plantations.
+                <br />
+                Ingredients that are common in most islands' dishes are rice, plantains, beans, cassava, cilantro, bell peppers, chickpeas, tomatoes, sweet potatoes, coconut, and any of various meats that are locally available like beef, poultry, pork or fish. 
+                Ingredients may include garlic, onions, scotch bonnet peppers, celery, green onions, and herbs like cilantro, Mexican mint, chives, marjoram, rosemary, tarragon and thyme. This green seasoning is used for a variety of dishes like curries, stews and roasted meats.
+                <br />
+                Traditional dishes are so important to regional culture that, for example, the local version of Caribbean goat stew has been chosen as the official national dish of Montserrat and is also one of the signature dishes of St. Kitts and Nevis. 
+                <br />
+                Over time, food from the Caribbean has evolved into a narrative technique through which their culture has been accentuated and promoted.
+            </p>
+            <img src="https://geology.com/world/caribbean-map.gif" />
         </div>
     )
 }

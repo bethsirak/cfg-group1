671057a9ac6cda08d714d941f4ac0aeb34c32102
Sophia John <smc_john@hotmail.com>
2023-08-21 22:45:39 -0400

    linked import './contact.css'; to contact.js

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -8,7 +8,6 @@ import Contact from './contact-page/contact';
 import TestComponent from './TestComponent';
 
 
-
 function App() {
   return (
     <Router>
@@ -27,66 +26,22 @@ function App() {
               <Link to="/about-us">About Us</Link>
             </li>
             <li>
-              <Link to="/contact">contact</Link>
+              <Link to="/contact">Contact</Link>
             </li>
             <li>
-              <Link to="/test">Test Component</Link> {/* Add this line */}
+              <Link to="/test">Test Component</Link>
             </li>
           </ul>
         </nav>
 
         <Routes>
           <Route path="/recipe-card" element={<RecipeCard />} />
-
-          <Route path="/about-us" element={<div className="App-ordered_list">
-            <h2>Anne</h2>
-            <ul>
-              <li>My favourite cuisine is Vietnamese but for desserts always head to Italy.</li>
-              <li>Outside the kitchen I like to run and cross-stitch.</li>
-              <li>I like running because it is a reason to go outside whils staying healthy.</li>
-            </ul>
-
-            <h2>Beth</h2>
-            <ul>
-              <li>I love watching tv shows and listening to music.</li>
-              <li>When Im not chilling in bed.. I go to boxing, something I have recently taken up.</li>
-              <li>I like boxing because it is active and requires working with others.</li>
-            </ul>
-
-            <h2>Georgia</h2>
-            <ul>
-              <li>My favourite food is Chinese but I do love a pizza!</li>
-              <li>My hobbies include rock climbing, snowboarding and playing with my rabbits.</li>
-              <li>I like rock climbing because it requires problem solving skills as well as physical strength.</li>
-            </ul>
-
-            <h2>Sophia</h2>
-            <ul>
-              <li>My favorite food is toast.</li>
-              <li>I like to read books.</li>
-              <li> I like reading because it has the potential to transport you to a whole new world.</li>
-            </ul>
-
-            <h2>Sarah</h2>
-            <ul>
-              <li>My favorite food is Mexican! As long as it is vegan.</li>
-              <li>I like to look after my plants, go charity shopping, and go jogging or to the gym.</li>
-              <li> I like looking after plants as it's interesting to learn about their different needs and they brighten up a room.</li>
-            </ul>
-
-            <h2>Grace</h2>
-            <ul>
-              <li>My favorite food is Thai, it is so flavourful and the perfect combination of taste profiles.</li>
-              <li>I spend a lot of time playing video games -</li>
-              <li>I love the way I use my brain when doing so, actively but also passively.</li>
-            </ul>
+          <Route path="/about-us" element={<div className="App-ordered_list">{/* ... */}</div>} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/test" element={<TestComponent />} />         
-          </div>} />
+          <Route path="/test" element={<TestComponent />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
 export default App;
--- my-first-app/src/contact-page/contact.js
+++ my-first-app/src/contact-page/contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './contact.css';
 
 function Contact() {
   function handleContactFormSubmit(event) {
@@ -29,8 +30,33 @@ function Contact() {
 
   return (
     <div>
-      {/* Your contact page HTML and form components can be added here */}
-      {/* Be sure to add the appropriate HTML structure and form elements */}
+      <nav className="navbar navbar-light bg-light fixed-top">
+        {/* Navbar content */}
+      </nav>
+
+      <section className="container mt-5">
+        <div className="row">
+          {/* Left Column */}
+          <div className="col-md-6 text-white">
+            <h2>Contact Us</h2>
+            <p>Want to request a new feature, report a bug, or just say hi? Send a message using the form!</p>
+            <p>Join our newsletter</p>
+            <form id="newsletterForm" onSubmit={handleNewsletterFormSubmit}>
+              {/* Newsletter form elements */}
+            </form>
+          </div>
+          {/* Right Column */}
+          <div className="col-md-6 bg-warning text-white">
+            <h2>Contact Us</h2>
+            <form id="contactForm" onSubmit={handleContactFormSubmit}>
+              {/* Contact form elements */}
+            </form>
+            <div id="successMessage" style={{ display: 'none' }}>Thank you! Your message has been sent.</div>
+          </div>
+        </div>
+      </section>
+
+      {/* Include Bootstrap JS and other scripts as needed */}
     </div>
   );
 }

e91af7f59eefe4ba4a779346af02b7176bb2795b
Sophia John <smc_john@hotmail.com>
2023-08-23 04:01:06 -0400

    Almost there

--- my-first-app/src/contact-page/contact.js
+++ my-first-app/src/contact-page/contact.js
@@ -7,7 +7,6 @@ function Contact() {
   function handleContactFormSubmit(event) {
     event.preventDefault();
 
-    // Validate the form fields (e.g., check if fields are filled)
     var fullName = document.querySelector('.input[name="name"]').value;
     var email = document.querySelector('.input[name="email"]').value;
     var message = document.querySelector('.input[name="message"]').value;
@@ -17,27 +16,34 @@ function Contact() {
       return;
     }
 
-    // If the form is valid, you can send the form data using AJAX
-    // Display the success message
     setSuccessMessageVisible(true);
   }
 
   function handleNewsletterFormSubmit(event) {
     event.preventDefault();
-
-    // Handle newsletter subscription here (e.g., send email to server)
-    // Display a confirmation message to the user
     alert("Subscribed to the newsletter!");
   }
 
   return (
     <div className="container">
       <div className="form">
-        <div className="contact-info"></div>
+        <div className="contact-info">
+          <h3 className="title">Contact Us</h3>
+          <p className="text">
+            Want to request a new feature, report a bug, or just say hi? Send a message using the form!
+          </p>
+          
+          <form id="newsletterForm" onSubmit={handleNewsletterFormSubmit} className="newsletter-form">
+            <div className="input-container">
+              <p className="newsletter-text">join our newsletter</p>
+              <input type="email" name="newsletterEmail" id="newsletterEmail" className="input input-newsletter" placeholder="Email Address" />
+              <button type="submit" className="btn-newsletter">Join</button>
+            </div>
+          </form>
+        </div>
 
         <div className="contact-form">
           <form onSubmit={handleContactFormSubmit}>
-            <h3 className="title">Contact Us</h3>
             <div className="input-container">
               <label htmlFor="name">FULL NAME :</label>
               <input type="text" name="name" className="input" />
@@ -50,7 +56,7 @@ function Contact() {
               <label htmlFor="message">MESSAGE :</label>
               <textarea name="message" className="input"></textarea>
             </div>
-            <button type="submit" className="btn btn-warning">Send</button>
+            <button type="submit" className="btn">Send</button>
           </form>
           {successMessageVisible && <div id="successMessage">Thank you! Your message has been sent.</div>}
         </div>

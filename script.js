ae48a26e78b4388074dacf8ed7cdf3965b484770
grackiee <137218837+grackiee@users.noreply.github.com>
2023-08-23 18:11:53 +0100

    Update script.js
    now has pop up! (says 'the wheel has stopped at undefined')

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function rotateFunction(){
     var min = 1024;
     var max = 9999;
-    var deg = Math.floor(Math.random() * (max - min)) + min;
-    document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+    var degr = Math.floor(Math.random() * (max - min)) + min;
+    document.getElementById('box').style.transform = "rotate("+degr+"deg)";
 }
 
 var element = document.getElementById('mainbox');
@@ -11,3 +11,30 @@ var element = document.getElementById('mainbox');
 setTimeout(function(){
   element.classList.add('animate');
   }, 5000);
+
+let box = document.querySelector(".box");
+let btn = document.getElementById("btn");
+  
+btn.onclick = function() {
+  let deg = Math.floor(Math.random() * 10000);
+  box.style.transition = "all 10s ease-out";
+  box.style.transform = `rotate(${deg}deg)`;
+  setTimeout(() => {
+    box.style.transition = "none";
+    let actualDeg = deg % 360;
+    box.style.transform = `rotate(${actualDeg}deg)`;
+  
+    let winningSlice = getWinningSlice(actualDeg);
+    if (winningSlice) {
+      let winningValue = winningSlice.dataset.value;
+      alert(`The spin wheel has stopped at ${winningValue}`);
+    }
+  }, 10000);
+  };
+  
+  function getWinningSlice(degrees) {
+    let slices = document.querySelectorAll(".box div");
+    let sliceAngle = 360 / slices.length;
+    let sliceIndex = Math.floor(degrees / sliceAngle);
+    return slices[sliceIndex];
+  }

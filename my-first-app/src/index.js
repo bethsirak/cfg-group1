110b78ebc9f4770aa8acfe9f750ea8f2a6a06c90
sloukelly <sarahloukelly@gmail.com>
2023-08-24 10:17:21 +0100

    Navbar files added
    Added the Navbar.jsx and Navbar.css and updated the index.js with the BrowserRouter wrap needed/ and imported it. Checked about.jsx and worldMap.jsx and correct export default function is included but can't see contact.js at the moment

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -5,15 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import "./Navbar.css";
+import {  BrowserRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
